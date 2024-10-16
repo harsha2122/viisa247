833d4195dd448e54a4af2cc06a5cf7284802d175
Harsha2122 <tsingh2214@gmail.com>
2024-10-16 19:39:45 +0530

    IP

--- src/app/helpers/axiosInstance.js
+++ src/app/helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Instance = axios.create({
   // baseURL: 'http://localhost:5003',
-  baseURL: 'http://13.233.165.152/',
+  baseURL: 'http://13.234.117.32/',
   credentials: 'include',
   withCredentials: true,
 })
